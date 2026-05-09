2381fff921fca58448a09f3a33c52f45e375ace3
vineet sakargayen <vineetisin@gmail.com>
2026-05-09 13:20:51 +0530

    Initial commit - project skeleton

--- src/routes/notificationRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { getNotifications } = require('../controllers/notificationController');
-const { requireReadAccess } = require('../middleware/auth');
-
-const router = express.Router();
-
-router.get('/', requireReadAccess, getNotifications);
-
-module.exports = router;
